73c082d62b94333f3da8cea4f7bf10952a9ff5b1
Shubhomoy <shubhomoy.tubu@gmail.com>
2018-12-26 16:17:42 +0530

    fixed namespace issue

--- reset.sh
+++ reset.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+kubectl delete -f deploy/service_account.yaml
 kubectl delete -f deploy/role.yaml
 kubectl delete -f deploy/role_binding.yaml
 kubectl delete -f deploy/operator.yaml
 
+kubectl create -f deploy/service_account.yaml
 kubectl create -f deploy/role.yaml
 kubectl create -f deploy/role_binding.yaml
 kubectl create -f deploy/operator.yaml
